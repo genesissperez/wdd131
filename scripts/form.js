dd892704eac6c518c4da765caacd2c50964a85da
genesissperez <erzaclaymore826@gmail.com>
2026-06-08 23:10:21 -0400

    w5-form

---
+++ scripts/form.js
@@ -0,0 +1,48 @@
+//Last modifications
+document.getElementById("lastModified").innerHTML = document.lastModified;
+lastModified.textContent = `📅 Last Modification: ${document.lastModified}`;
+
+//display the current year
+const year = document.querySelector("#currentyear");
+
+const today = new Date();
+
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
+
+const selectProduct = document.getElementById("product-name");
+
+products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.id;
+    option.textContent = product.name;
+    selectProduct.appendChild(option);
+})
---
+++ scripts/review.js
@@ -0,0 +1,24 @@
+//Last modifications
+document.getElementById("lastModified").innerHTML = document.lastModified;
+lastModified.textContent = `📅 Last Modification: ${document.lastModified}`;
+
+//display the current year
+const year = document.querySelector("#currentyear");
+
+const today = new Date();
+
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+//REVIEW
+
+document.addEventListener("DOMContentLoaded", () => {
+    let reviewCount = localStorage.getItem("reviewCount") || 0;
+    reviewCount++;
+
+    localStorage.setItem("reviewCount", reviewCount);
+
+    const counterElement = document.getElementById("counter-display");
+    if (counterElement) {
+        counterElement.textContent = reviewCount;
+    }
+});
